dcf400bff848021679f987b62602b820eacc81e2
Kristoffer <deceiver.g@gmail.com>
2008-09-20 21:07:43 +0200

    Fixed issues in update.sh, added epylint script.

--- update.sh
+++ update.sh
@@ -3,14 +3,26 @@ case $1 in
 pull)
 	Message="Pulled latest data from local setup."
 	cp $HOME/.emacs ./dot_emacs
-	cp -r $HOME/.emacs.d ./emacs.d
+	if [ ! -d ./emacs.d ] ; then
+		mkdir ./emacs.d
+	fi
+	cp -r $HOME/.emacs.d/* ./emacs.d/
 	cp $HOME/.Xresources ./Xresources
+	cp $HOME/bin/epylint ./
 	;;
 install)
 	Message="Installed from repo into home directory."
 	cp ./dot_emacs $HOME/.emacs
-	cp -r ./emacs.d $HOME/.emacs.d
+	if [ ! -d $HOME/.emacs.d ] ; then
+		mkdir $HOME/.emacs.d
+	fi
+	cp -r ./emacs.d/* $HOME/.emacs.d/
 	cp ./Xresources $HOME/.Xresources
+	if [ ! -d $HOME/bin ] ; then
+		mkdir $HOME/bin
+		Message="Installed from repo into home directory. Add ~/bin to your path."
+	fi
+	cp ./epylint $HOME/bin/
 	;;
 *)
 	Message="Unknown command (pull, install)"
