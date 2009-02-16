1ea2ba5dee7560716a64e373439c2419d002973c
Kristoffer Grönlund <deceiver.g@gmail.com>
2009-02-16 07:57:23 +0100

    Cleaned up update.sh a bit

--- update.sh
+++ update.sh
@@ -3,9 +3,7 @@ case $1 in
 pull)
 	Message="Pulled latest data from local setup."
 	cp $HOME/.emacs ./dot_emacs
-	if [ ! -d ./emacs.d ] ; then
-		mkdir ./emacs.d
-	fi
+	mkdir -p ./emacs.d
 	cp -r $HOME/.emacs.d/* ./emacs.d/
 	cp $HOME/.Xresources ./Xresources
 	cp $HOME/bin/epylint ./
@@ -21,15 +19,10 @@ colors)
 install)
 	Message="Installed from repo into home directory."
 	cp ./dot_emacs $HOME/.emacs
-	if [ ! -d $HOME/.emacs.d ] ; then
-		mkdir $HOME/.emacs.d
-	fi
+	mkdir -p $HOME/.emacs.d
 	cp -r ./emacs.d/* $HOME/.emacs.d/
 	cp ./Xresources $HOME/.Xresources
-	if [ ! -d $HOME/bin ] ; then
-		mkdir $HOME/bin
-		Message="Installed from repo into home directory. Add ~/bin to your path."
-	fi
+	mkdir $HOME/bin
 	cp ./epylint $HOME/bin/
 	;;
 *)
