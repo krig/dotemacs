42ba5df7be215c7b437e455955c30b83a42c205d
Kristoffer Grönlund <kristoffer.gronlund@purplescout.se>
2009-11-13 20:30:10 +0100

    Improved calendar for awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -118,17 +118,24 @@ function remove_calendar()
    end
 end
 
+function trim(s)
+   return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
+end
+
 function add_calendar(inc_offset)
    local save_offset = offset
    remove_calendar()
    offset = save_offset + inc_offset
    local datespec = os.date("*t")
+   local day = datespec.day
    datespec = datespec.year * 12 + datespec.month - 1 + offset
    datespec = (datespec % 12 + 1) .. " " .. math.floor(datespec / 12)
-   local cal = awful.util.pread("cal -m " .. datespec)
-   cal = string.gsub(cal, "^%s*(.-)%s*$", "%1")
+   local cal = awful.util.pread("ncal -w " .. datespec)
+   cal = trim(cal)
+   local sday = tostring(day)
+   cal = string.gsub(cal, "( "..sday..")", "<span foreground=\"red\"><b>%1</b></span>")
    local setup = {
-      text = string.format('<span font_desc="%s">%s</span>', "monospace", os.date("%a, %d %B %Y") .. "\n" .. cal),
+      text = string.format('<span font_desc="%s">%s</span>', "monospace", "<b>" .. os.date("%A %d") .. "</b>\n" .. cal),
       timeout = 0, hover_timeout = 0.5,
       width = 160,
    }
