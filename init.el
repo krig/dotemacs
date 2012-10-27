4693828c8df6d0be26af29595e9f043ada3eed21
Kristoffer Grönlund <krig@koru.se>
2012-10-27 21:53:28 +0200

    various updates

--- init.el
+++ init.el
@@ -656,9 +656,12 @@
 (autoload 'flex-mode "flex-mode.el")
 (add-to-list 'auto-mode-alist '("\\.l\\'" . flex-mode))
 
-(load "~/.emacs.d/haskell-mode-2.8.0/haskell-site-file")
+;;(load "~/.emacs.d/haskell-mode-2.8.0/haskell-site-file")
+(load "~/.emacs.d/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+
 
 ;; ;; PARROT
 
