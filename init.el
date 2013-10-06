4342b646a6489e958dd8e638f4b95b6b551402c2
Kristoffer Grönlund <krig@koru.se>
2013-10-06 22:05:08 +0200

    fighting scroll bars in linux

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
   (if (string-match "kowloon" hostname)
       (set-frame-font "Ubuntu Mono-14")
     (set-frame-font "Ubuntu Mono-12"))
-  (menu-bar-mode 0))
+  (menu-bar-mode -1))
 
 (setq custom-safe-themes '("2233263f8185428aa9c6df1d32353cff86f09ec8a008983c9f799f4efc341b31" "bb27775d3f6e75ea0faa855ecf3eea6744e0951378474f9a3e29908f3fdfb3cd" "36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" default))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
@@ -67,7 +67,7 @@
 
 (delete-selection-mode t)
 ;;(setq delete-active-region nil)
-(set-scroll-bar-mode nil)
+(scroll-bar-mode -1)
 (tool-bar-mode -1)
 (blink-cursor-mode t)
 (show-paren-mode t)
