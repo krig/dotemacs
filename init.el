4335d115ccdce9017a77c2ebb48e13652086611c
Kristoffer Grönlund <krig@koru.se>
2014-08-28 18:35:57 +0200

    re-enable the GNU and marmalade package repos

--- init.el
+++ init.el
@@ -126,8 +126,8 @@
 ;; ELPA
 (require 'package)
 (setq package-archives '(;("ELPA" . "http://tromey.com/elpa/")
-                         ;("gnu" . "http://elpa.gnu.org/packages/")
-                         ;("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (require 'el-get)
