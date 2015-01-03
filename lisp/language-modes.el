22035067aa2e0c78076bd25aab82db08ed6cd4d6
Kristoffer Grönlund <krig@koru.se>
2015-01-03 23:22:26 +0100

    auto-complete-mode: tried it and hate it.

--- lisp/language-modes.el
+++ lisp/language-modes.el
@@ -313,7 +313,17 @@ the name of FILE in the current directory, suitable for creation"
   (add-to-list 'load-path "~/.emacs.d/modes/auto-complete")    ; This may not be appeared if you have already added.
   (require 'auto-complete-config)
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-  (ac-config-default))
+
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup))
+  ;;(global-auto-complete-mode t))
+
+; nope, I hate global auto-complete-mode
+  ;(ac-config-default))
 
 ;; nimrod mode
 (progn
