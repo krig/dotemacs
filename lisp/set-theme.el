28115eed32c0fadcd35d174227922bc05764a0fe
Kristoffer Grönlund <krig@koru.se>
2015-06-17 00:54:15 +0200

    tweaked font setting

--- lisp/set-theme.el
+++ lisp/set-theme.el
@@ -1,26 +1,40 @@
 ;; loads and sets the theme
 
+(require 'cl)
+
+(defun font-size-for-machine ()
+   (pcase (car (split-string hostname "\\."))
+     ("walker" 15)
+     ("kowloon" 17)
+     ("krigpad" 13)
+     ("ultralix" 17)
+     (_ 14)))
+
+(defun font-candidate (&rest fonts)
+  "Return existing font which first match."
+  (let ((fonts (map 'list (lambda (f) (format "%s-%d:weight=normal" f (font-size-for-machine))) fonts)))
+    (find-if (lambda (f) (find-font (font-spec :name f))) fonts)))
+
 ;; set font
 (when (display-graphic-p)
   (when (krig-macp)
     (setq mac-allow-anti-aliasing t)
-    (if (> (display-pixel-width) 1900)
-        (set-frame-font "Inconsolata-15")
-      (set-frame-font "Inconsolata-13"))
-                                        ; nicer lambdas
+    (set-frame-font (format "Inconsolata-%d" (if (> (display-pixel-width) 1900) 15 13)))
     (set-fontset-font "fontset-default"
                       'greek-iso8859-7
                       '("Consolas" . "iso10646-1")))
 
   (when (krig-linuxp)
-    (set-frame-font
-     (pcase (car (split-string hostname "\\."))
-       ("walker" "Ubuntu Mono-15")
-       ("kowloon" "Ubuntu Mono-17")
-       ("krigpad" "Ubuntu Mono-13")
-       ("ultralix" "Ubuntu Mono-17")
-       (_ "Ubuntu Mono-14")))
-    (menu-bar-mode -1)))
+    (let ((font (font-candidate
+                 "Fantasque Sans Mono"
+                 "Ubuntu Mono"
+                 "Inconsolata"
+                 "Consolas"
+                 "Liberation Mono"
+                 "DejaVu Sans Mono")))
+      (message "Setting font: %s" font)
+      (set-face-attribute 'default nil :font font)))
+  (menu-bar-mode -1))
 
 ;; work around the scrollbar bug
 (when (display-graphic-p)
