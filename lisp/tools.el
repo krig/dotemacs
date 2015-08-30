50642df3e50ec6f0b8a9616f9efd2a3c996b9fd0
Kristoffer Grönlund <krig@koru.se>
2015-08-30 19:38:11 +0200

    Make magit-status run fullscreen in frame
    Restores old window configuration when quitting.

--- lisp/tools.el
+++ lisp/tools.el
@@ -210,3 +210,23 @@ symbol, not word, as I need this for programming the most."
 (add-to-list 'load-path "~/.emacs.d/tools/textmate")
 (require 'textmate)
 (textmate-mode)
+
+
+;; magit
+;; http://whattheemacsd.com/setup-magit.el-01.html
+;; full screen magit-status
+
+
+(when (require 'magit nil 'noerror)
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
+  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
