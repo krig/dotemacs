52ef0e0e36889ffefbbf7c245108498eec704c59
Kristoffer Grönlund <krig@koru.se>
2015-08-30 17:09:45 +0200

    in-progress variant of noctilux charmingly named proctologist-theme

---
+++ themes/proctologist-theme.el
@@ -0,0 +1,9 @@
+;;; proctologist-theme.el --- Dark theme inspired by LightTable
+
+(require 'proctologist-definitions
+         (locate-file "proctologist-definitions.el" custom-theme-load-path
+                      '("c" "")))
+
+(create-proctologist-theme)
+
+;;; proctologist-theme.el ends here
